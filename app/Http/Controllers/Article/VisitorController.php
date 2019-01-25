aa0ecaac68daba1edf28ccf02bad0c587455ded0
huangzhongxi <huangzhongxi@yundun.com>
2019-01-25 10:03:13 +0800

    修复文章访问和评论

--- app/Http/Controllers/Article/VisitorController.php
+++ app/Http/Controllers/Article/VisitorController.php
@@ -12,23 +12,54 @@ use App\Http\Controllers\ResetController;
 use Illuminate\Http\Request;
 use App\Model\ArticleVisitor;
 use App\Model\Article;
+use App\Model\IPInfo;
 
 class VisitorController extends ResetController
 {
    public function index(Request $request)
    {
-        $query = ArticleVisitor::query()->rightJoin('ip_infos', 'article_visitors.ip', 'ip_infos.ip');
+        $query = ArticleVisitor::query();
 
         if(!empty($request->get('article_id'))){
             $query->where('article_id', '=', $request->get('article_id'));
         }
 
+       if(!empty($request->get('ip'))){
+           $query->where('ip', '=', $request->get('ip'));
+       }
        $limit = !empty($request->get('limit')) ? $request->get('limit') : 25;
        $rows = $query->orderBy('article_visitors.id', 'desc')->paginate($limit);
        $rows = $rows->appends($request->all());
 
+       if($rows->count()){
+         $ip_data =  $this->getIpLocalList($rows);
+       }else{
+           $ip_data = [];
+       }
+
        $article = Article::query()->where('status', '=', 1)->where('is_draft', '=', 0)->get();
 
-       return view('article.visitor-index')->with(['rows' => $rows, 'pageSizes' => $this->pageSizes, 'articles' => $article]);
+       return view('article.visitor-index')->with(['rows' => $rows, 'pageSizes' => $this->pageSizes, 'articles' => $article, 'ip_data' => $ip_data]);
+   }
+
+
+   protected function getIpLocalList($rows)
+   {
+       $ips = $data = [];
+        foreach($rows as $row){
+            $ips[] = $row->ip;
+        }
+
+        $ips = array_unique($ips);
+
+       $ip_list = IPInfo::query()->whereIn('ip', $ips)->get()->toArray();
+
+       if($ip_list){
+           foreach($ip_list as $key=> $val){
+               $data[$val['ip']] = $val;
+           }
+       }
+
+        return $data;
    }
 }
